7ae201371eee82ce267a2b9120925c59955cd648
joycej3 <joycej3@tcd.ie>
2023-04-04 17:53:40 +0100

    added wifi to door (rfid)

--- rfid/main/main.c
+++ rfid/main/main.c
@@ -10,10 +10,120 @@
 #include "freertos/semphr.h"
 #include "driver/ledc.h"
 #include "sdkconfig.h"
-#include "esp_adc/adc_continuous.h"
 
 
-static const char* TAG = "rc522";
+#include <string.h>
+#include <sys/param.h>
+
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+
+#include "nvs_flash.h"
+#include "esp_netif.h"
+#include "protocol_examples_common.h"
+#include "addr_from_stdin.h"
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+
+
+// #include "py/compile.h"
+// #include "py/runtime.h"
+// #include "py/gc.h"
+// #include "py/mpstate.h"
+// #include "py/mphal.h"
+// #include "client.py"##
+
+#if defined(CONFIG_EXAMPLE_IPV4)
+#define HOST_IP_ADDR CONFIG_EXAMPLE_IPV4_ADDR
+#elif defined(CONFIG_EXAMPLE_IPV6)
+#define HOST_IP_ADDR CONFIG_EXAMPLE_IPV6_ADDR
+#else
+#define HOST_IP_ADDR "172.20.10.9 8080"
+#endif
+
+#define PORT CONFIG_EXAMPLE_PORT
+
+static const char *TAG = "example";
+static const char *payload = "Door Open";
+
+static void tcp_client_task(void *pvParameters)
+{
+    char rx_buffer[128];
+    char host_ip[] = HOST_IP_ADDR;
+    int addr_family = 0;
+    int ip_protocol = 0;
+
+    while (1) {
+#if defined(CONFIG_EXAMPLE_IPV4)
+        struct sockaddr_in dest_addr;
+        dest_addr.sin_addr.s_addr = inet_addr(host_ip);
+        dest_addr.sin_family = AF_INET;
+        dest_addr.sin_port = htons(PORT);
+        addr_family = AF_INET;
+        ip_protocol = IPPROTO_IP;
+#elif defined(CONFIG_EXAMPLE_IPV6)
+        struct sockaddr_in6 dest_addr = { 0 };
+        inet6_aton(host_ip, &dest_addr.sin6_addr);
+        dest_addr.sin6_family = AF_INET6;
+        dest_addr.sin6_port = htons(PORT);
+        dest_addr.sin6_scope_id = esp_netif_get_netif_impl_index(EXAMPLE_INTERFACE);
+        addr_family = AF_INET6;
+        ip_protocol = IPPROTO_IPV6;
+#elif defined(CONFIG_EXAMPLE_SOCKET_IP_INPUT_STDIN)
+        struct sockaddr_storage dest_addr = { 0 };
+        ESP_ERROR_CHECK(get_addr_from_stdin(PORT, SOCK_STREAM, &ip_protocol, &addr_family, &dest_addr));
+#endif
+        int sock =  socket(addr_family, SOCK_STREAM, ip_protocol);
+        if (sock < 0) {
+            ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+            break;
+        }
+        ESP_LOGI(TAG, "Socket created, connecting to %s:%d", host_ip, PORT);
+
+        int err = connect(sock, (struct sockaddr *)&dest_addr, sizeof(struct sockaddr_in6));
+        if (err != 0) {
+            ESP_LOGE(TAG, "Socket unable to connect: errno %d", errno);
+            break;
+        }
+        ESP_LOGI(TAG, "Successfully connected");
+
+        while (1) {
+        int err = send(sock, payload, strlen(payload), 0);
+        if (err < 0) {
+            ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
+            break;
+        }
+
+        int len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
+        // Error occurred during receiving
+        if (len < 0) {
+            ESP_LOGE(TAG, "recv failed: errno %d", errno);
+            break;
+        }
+        // Data received
+        else {
+            rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string
+            ESP_LOGI(TAG, "Received %d bytes from %s:", len, host_ip);
+            ESP_LOGI(TAG, "%s", rx_buffer);
+            break;
+        }
+
+        // vTaskDelay(2000 / portTICK_PERIOD_MS);
+        }
+
+        if (sock != -1) {
+            ESP_LOGE(TAG, "Shutting down socket and restarting...");
+            shutdown(sock, 0);
+            close(sock);
+        }
+    }
+    vTaskDelete(NULL);
+}
+
+
+static const char* nfc_TAG = "rc522";
 ;
 static rc522_handle_t scanner;
 long long int allowed_tag = 967306403452;
@@ -82,18 +192,19 @@ static void rc522_handler(void* arg, esp_event_base_t base, int32_t event_id, vo
     switch(event_id) {
         case RC522_EVENT_TAG_SCANNED: {
                 rc522_tag_t* tag = (rc522_tag_t*) data->ptr;
-                ESP_LOGI(TAG, "Tag scanned (sn: %" PRIu64 ")", tag->serial_number);
+                ESP_LOGI(nfc_TAG, "Tag scanned (sn: %" PRIu64 ")", tag->serial_number);
                 if(tag->serial_number == allowed_tag ){
-                    ESP_LOGI(TAG, "Access Granted");
+                    ESP_LOGI(nfc_TAG, "Access Granted");
 
                     
 
                     xTaskCreate(&sweepServo_task,"sweepServo_task",2048,NULL,5,NULL);
+                    xTaskCreate(tcp_client_task, "tcp_client", 4096, NULL, 5, NULL);
 
-                    ESP_LOGI(TAG, "Door Closed");
+                    ESP_LOGI(nfc_TAG, "Door Closed");
                 }
                 else{
-                    ESP_LOGI(TAG, "Access Denied");
+                    ESP_LOGI(nfc_TAG, "Access Denied");
 
                 }
 
@@ -103,6 +214,18 @@ static void rc522_handler(void* arg, esp_event_base_t base, int32_t event_id, vo
 }
 
 void app_main(){
+
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+    ESP_ERROR_CHECK(example_connect());
+
+    
   
     rc522_config_t config = {
         .spi.host = VSPI_HOST,
@@ -111,6 +234,8 @@ void app_main(){
         .spi.sck_gpio = 19,
         .spi.sda_gpio = 22,
     };
+    
+   
 
     
     // nvs_flash_init();
